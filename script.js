b04fd9c11f0dbf6db1eac2f9da1d970655b67e13
Triplane60 <jambyletesa@gmail.com>
2026-03-06 21:39:52 +0800

    update

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function addToCart(watchName) {
-    alert(watchName + " has been added to your cart!");
-    console.log("Cart updated with: " + watchName);
+function filterCategory(category) {
+    const cards = document.querySelectorAll('.watch-card');
+    const buttons = document.querySelectorAll('.nav-item');
+
+
+    buttons.forEach(btn => btn.classList.remove('active'));
+    event.target.classList.add('active');
+
+    cards.forEach(card => {
+        if (category === 'all') {
+            card.style.display = 'block'; 
+        } else if (card.classList.contains(category)) {
+            card.style.display = 'block'; 
+        } else {
+            card.style.display = 'none';  
+        }
+    });
 }
